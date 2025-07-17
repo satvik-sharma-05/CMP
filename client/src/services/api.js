cdb0631069a81b921379d5313702af242c2d2e01
Satvik Sharma <sharmasatvik031@gmail.com>
2025-07-17 15:36:50 +0530

    Set production API URL and rebuild

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const API_URL = import.meta.env.VITE_API_URL
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 console.log("API_URL =", API_URL);
+
 
 const api = axios.create({
   baseURL: API_URL,
